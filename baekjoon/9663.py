d55132d58f4e66b739a766e39b5c5f2cdbb0df0f
jjjjjeongwon <jjjjjwon21@gmail.com>
2023-04-12 21:18:03 +0900

    complete

---
+++ baekjoon/9663.py
@@ -0,0 +1,33 @@
+import sys
+
+n = int(input())
+
+count = 0
+
+
+def n_queens(col, i):
+    global count
+
+    if (promising(col, i)):
+        if (i == n):
+            count += 1
+
+        else:
+            for j in range(1, n+1):
+                col[i+1] = j
+                n_queens(col, i+1)
+
+
+def promising(col, i):
+    k = 1
+    while (k < i):
+        if (col[i] == col[k] or abs(col[i]-col[k]) == (i-k)):
+            return False
+        k += 1
+    return True
+
+
+col = [0]*(n+1)
+n_queens(col, 0)
+
+print(count)
