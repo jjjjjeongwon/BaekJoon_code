34c6e4980961566e13e88a3490f478c7ab5c6528
jjjjjeongwon <jjjjjwon21@gmail.com>
2023-04-23 00:12:00 +0900

    complete

--- baekjoon/16120.py
+++ baekjoon/16120.py
@@ -9,7 +9,6 @@ a = input().strip()
 for i in range(len(a)):
     stack.append(a[i])
     stack_size += 1
-    # print(stack_size)
-    print(stack)
     if stack[i-4:i] == ['P', 'P', 'A', 'P']:
         print(i)
+        stack_size =
