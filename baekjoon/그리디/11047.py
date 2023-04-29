acb21f96ec90e8c05ba1f00070e6b59fbd6d0de5
jjjjjeongwon <jjjjjwon21@gmail.com>
2023-04-29 23:14:08 +0900

    complete

--- baekjoon/그리디/11047.py
+++ baekjoon/그리디/11047.py
@@ -6,7 +6,6 @@ coinList = [int(input()) for i in range(N)]
 countCoin = 0
 
 for i in range(N, 0, -1):
-    if K // coinList[i-1] > 0:
-        countCoin += K//coinList[i-1]
-        K = K - (K//coinList[i-1])*coinList[i-1]
+    countCoin += K//coinList[i-1]
+    K = K % coinList[i-1]
 print(countCoin)
---
+++ baekjoon/그리디/1931.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+timetable = []
+
+for i in range(N):
+    s, e = list(map(int, input().split()))
+    timetable.append([s, e])
+
+timetable.sort(key=lambda x: (x[1], x[0]))
+
+count = 0
+lastEnd = 0
+for i, j in timetable:
+    if lastEnd <= i:
+        count += 1
+        lastEnd = j
+
+print(count)
